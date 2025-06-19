7b0dcecece8723d3f5ceda5a44fb4778c6cda97f
AJAS M M <ajasmm02@gmail.com>
2025-06-19 17:33:42 +0530

    seperate folder for platforms

--- BSKModing/Assets/Moding/Scripts/AssetBundleBuilder.cs
+++ BSKModing/Assets/Moding/Scripts/AssetBundleBuilder.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using Unity.Android.Gradle;
 
 
 public static class AssetBundleBuilder
@@ -20,12 +21,12 @@ public static class AssetBundleBuilder
 
     private static void Build(BuildTarget buildTarget)
     {
-        string assetBundleDirectory = "Assets/AssetBundles";
+        string assetBundleDirectory = $"Assets/AssetBundles/{(buildTarget == BuildTarget.Android ? "Android" : "Windows")}";
         if(!Directory.Exists(assetBundleDirectory))
             Directory.CreateDirectory(assetBundleDirectory);
 
         BuildPipeline.BuildAssetBundles(assetBundleDirectory,
-                                        BuildAssetBundleOptions.None,
+                                        BuildAssetBundleOptions.ForceRebuildAssetBundle,
                                         buildTarget);
     }
 }
